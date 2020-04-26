424891d5356e30069bbc4773183d7179981b1e07
sametsafkan <D6cf53ec36>
2020-04-26 15:07:35 +0300

    added state machine transition configuration.

--- src/main/java/com/sametsafkan/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/com/sametsafkan/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
+import static com.sametsafkan.beer.order.service.domain.BeerOrderEventEnum.*;
 import static com.sametsafkan.beer.order.service.domain.BeerOrderStatusEnum.*;
 
 @Slf4j
@@ -27,5 +29,17 @@ public class BeerOrderStateMachineConfig extends
                 .end(DELIVERY_EXCEPTION)
                 .end(VALIDATION_EXCEPTION)
                 .end(ALLOCATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+        transitions.withExternal()
+                .source(NEW).target(NEW).event(VALIDATE_ORDER)
+                .and()
+                .withExternal()
+                .source(NEW).target(VALIDATED).event(VALIDATION_PASSED)
+                .and()
+                .withExternal()
+                .source(NEW).target(VALIDATION_EXCEPTION).event(VALIDATION_FAILED);
     }
 }
